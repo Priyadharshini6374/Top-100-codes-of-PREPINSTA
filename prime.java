8d18a20f78ed60756db9836cdace69bcd5055222
Priyadharshini K <155759682+Priyadharshini6374@users.noreply.github.com>
2025-04-23 13:30:27 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,20 @@
+// 23.04.25 -- 01.19pm
+
+import java.util.Scanner;
+public class prime{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int number = scan.nextInt();
+        int count =0;
+        for(int i=2;i<number;i++){
+            if(number%i==0){
+                System.out.println("not a prime and the factors are:" +i);
+                count++;
+            }
+        }
+        if(count==0){
+            System.out.print("Its a prime number");
+        }
+    }
+}
